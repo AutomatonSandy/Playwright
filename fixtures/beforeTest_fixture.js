4bd23175cb2bf0a80991a7aabd633d78ada10baf
Sandipan Bhattacharjee <sandipanbhattacharjee1981@gmail.com>
2026-02-24 22:01:36 -0800

    Modifying fixture file and .yml github actions file

--- fixtures/beforeTest_fixture.js
+++ fixtures/beforeTest_fixture.js
@@ -3,9 +3,7 @@ import{test as setup} from '@playwright/test';
 export const test = setup.extend({
     page: async ({ page }, use) => {
         await page.goto("https://the-internet.herokuapp.com/");
-        console.log("Before test");
         await use(page);
-        console.log("After test");
         await page.goto("https://the-internet.herokuapp.com/");
     }
 });
